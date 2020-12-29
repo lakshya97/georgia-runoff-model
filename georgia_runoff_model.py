a8a03de4c27ab20907bc06f61261704f65d4f8c9
LAKSHYA JAIN <Lakshya.jain@berkeley.edu>
2020-12-29 01:19:00 -0800

    refactoring to be cleaner, also allowing for modifiers

--- georgia_runoff_model.py
+++ georgia_runoff_model.py
@@ -9,6 +9,9 @@ state_df = pd.read_csv("general_election_data.csv")
 counties = sorted(state_df["County"].unique())
 county_id = 0
 results_df = None
+vbm_modifier = 1.0
+adv_modifier = 1.0
+eday_modifier = 1.0
 
 def compute_list_difference(a, b):
     count = Counter(a) # count items in a
@@ -31,20 +34,31 @@ for county_name in counties:
             single_county_df = single_county_df.replace(to_replace={'County Precinct': str(newton_id)}, value={'County Precinct': '{:0>2}'.format(newton_id)})
 
     single_county_df["County Precinct"] = single_county_df["County Precinct"].astype(str)
+
     ## create a new column that says 1 if the ballot is validly cast, 0 if not
-    single_county_df["isValid"] = single_county_df["Ballot Status"] == "A"
+    single_county_df["isValid"] = 0
+    single_county_df.loc[single_county_df["Ballot Status"] == "A", "isValid"] = 1
+
     single_county_df["Ballot Style"] = single_county_df["Ballot Style"].replace("ELECTRONIC", "MAILED")
+
+    # adjust for how many vbm ballots are projected (default: no adjustment)
+    single_county_df.loc[single_county_df["Ballot Style"] == "MAILED", "isValid"] *= vbm_modifier
+
+    # adjust for how many in-person ballots are projected (default: no adjustment)
+    single_county_df.loc[single_county_df["Ballot Style"] == "IN PERSON", "isValid"] *= adv_modifier
 
     county_accept_totals = single_county_df.groupby(["County Precinct", "Ballot Style"]).size().reset_index(name='num_applications')
 
     ## create a count that sums up the valid votes cast and splits by style
     county_votes_cast_split = single_county_df.groupby(["County Precinct", "Ballot Style"], as_index=False).sum("isValid")
+    # print(county_name)
     county_votes_cast_totals = single_county_df[single_county_df["isValid"] == 1].groupby(["County Precinct"]).size().reset_index(name='total_votes')
 
+    # I don't really use this, but it collects info on the VBM acceptance rate. Not really needed right now, though.
+    # county_totals_and_rates = county_votes_cast_split
     county_totals_and_rates = county_votes_cast_split.merge(county_accept_totals, left_on=["County Precinct", "Ballot Style"], right_on=["County Precinct", "Ballot Style"], how="inner")
     county_totals_and_rates["acceptance rate"] = county_totals_and_rates["isValid"]/county_totals_and_rates["num_applications"]
 
-    # results_df = state_df[["County", "Precinct"]]
     county_df = state_df[state_df["County"] == county_name]
 
     # Early Voting Rate
@@ -93,7 +107,7 @@ for county_name in counties:
     ev_total = ev_total[ev_total["Ballot Style"] == "IN PERSON"]
     ev_df = ev_total[["County", "Precinct", "dem_eday_share"]]
 
-    ev_df["proj_eday_votes"] = ev_total["election_day_vote_rate"] * ev_total["total_votes"]
+    ev_df["proj_eday_votes"] = ev_total["election_day_vote_rate"] * ev_total["total_votes"] * eday_modifier
     ev_df["dem_eday_votes"] = ev_df["dem_eday_share"] * ev_df["proj_eday_votes"]
     ev_df["gop_eday_votes"] = ev_df["proj_eday_votes"] - ev_df["dem_eday_votes"]
 
@@ -108,27 +122,71 @@ for county_name in counties:
     else:
         results_df = pd.concat([results_df, county_result_df], ignore_index=True)
 
+
+## HERE IS WHERE WE DO ALL THE REPORTING OF THE DATA
+
 dem_votes = results_df["Dem Total Votes"].sum()
 gop_votes = results_df["GOP Total Votes"].sum()
 
-state_df["tuple_set"] = "(" + state_df["County"] + "," + state_df["Precinct"] + ")"
-results_df["tuple_set"] = "(" + results_df["County"] + "," + results_df["Precinct"] + ")"
-output_counties = sorted(results_df["County"].unique())
+dem_vbm_share = results_df["dem_vbm_votes"].sum()/(results_df["dem_vbm_votes"].sum() + results_df["gop_vbm_votes"].sum())
+gop_vbm_share = results_df["gop_vbm_votes"].sum()/(results_df["dem_vbm_votes"].sum() + results_df["gop_vbm_votes"].sum())
 
-print("DEM VOTES", dem_votes)
-print("GOP VOTES", gop_votes)
+print("-------------------")
+print("Dem VBM Share: ", round(dem_vbm_share * 100, 2))
+print("GOP VBM Share: ", round(gop_vbm_share * 100, 2))
+print("Dem VBM Votes: ", round(results_df["dem_vbm_votes"].sum(), 0))
+print("GOP VBM Votes: ", round(results_df["gop_vbm_votes"].sum(), 0))
+print("Total VBM Votes: ", round(results_df["gop_vbm_votes"].sum() + results_df["dem_vbm_votes"].sum(), 0))
+print("-------------------\n")
 
-print("Dem VBM Share: ", results_df["dem_vbm_votes"].sum()/(results_df["dem_vbm_votes"].sum() + results_df["gop_vbm_votes"].sum()))
-print("GOP VBM Share: ", results_df["gop_vbm_votes"].sum()/(results_df["dem_vbm_votes"].sum() + results_df["gop_vbm_votes"].sum()))
-print("Dem ADV Share: ", results_df["dem_adv_votes"].sum()/(results_df["dem_adv_votes"].sum() + results_df["gop_adv_votes"].sum()))
-print("GOP ADV Share: ", results_df["gop_adv_votes"].sum()/(results_df["dem_adv_votes"].sum() + results_df["gop_adv_votes"].sum()))
+dem_adv_share = results_df["dem_adv_votes"].sum()/(results_df["dem_adv_votes"].sum() + results_df["gop_adv_votes"].sum())
+gop_adv_share = results_df["gop_adv_votes"].sum()/(results_df["dem_adv_votes"].sum() + results_df["gop_adv_votes"].sum())
+
+print("-------------------")
+print("Dem ADV Share: ", round(dem_adv_share * 100, 2))
+print("GOP ADV Share: ", round(gop_adv_share * 100, 2))
+print("Dem ADV Votes: ", round(results_df["dem_adv_votes"].sum(), 0))
+print("GOP ADV Votes: ", round(results_df["gop_adv_votes"].sum(), 0))
+print("Total ADV Votes: ", round(results_df["gop_adv_votes"].sum() + results_df["dem_adv_votes"].sum(), 0))
+print("-------------------\n")
 
 total_early_votes = results_df["dem_vbm_votes"].sum() + results_df["gop_vbm_votes"].sum() + results_df["dem_adv_votes"].sum() + results_df["gop_adv_votes"].sum()
+dem_early_votes = results_df["dem_vbm_votes"].sum() + results_df["dem_adv_votes"].sum()
+gop_early_votes = results_df["gop_vbm_votes"].sum() + results_df["gop_adv_votes"].sum()
+dem_early_share = dem_early_votes/total_early_votes
+gop_early_share = gop_early_votes/total_early_votes
 
-print("Dem Total Early share:", (results_df["dem_vbm_votes"].sum() + results_df["dem_adv_votes"].sum())/total_early_votes)
-print("GOP Total Early share:", (results_df["gop_vbm_votes"].sum() + results_df["gop_adv_votes"].sum())/total_early_votes)
-print("Dem Election Day Share: ", results_df["dem_eday_votes"].sum()/(results_df["dem_eday_votes"].sum() + results_df["gop_eday_votes"].sum()))
-print("GOP Election Day Share: ", results_df["gop_eday_votes"].sum()/(results_df["dem_eday_votes"].sum() + results_df["gop_eday_votes"].sum()))
-print("DEM Share", dem_votes/(dem_votes + gop_votes))
-print("GOP Share", gop_votes/(dem_votes + gop_votes))
+print("-------------------")
+print("Dem Total Early Share:", round(dem_early_share * 100, 2))
+print("GOP Total Early Share:", round(gop_early_share * 100, 2))
+print("Dem Total Early Votes", round(dem_early_votes, 0))
+print("GOP Total Early Votes", round(gop_early_votes, 0))
+print("Total Early Votes: ", round(total_early_votes, 0))
+print("-------------------\n")
 
+total_eday_votes = results_df["dem_eday_votes"].sum() + results_df["gop_eday_votes"].sum()
+dem_eday_share = results_df["dem_eday_votes"].sum()/total_eday_votes
+gop_eday_share = results_df["gop_eday_votes"].sum()/total_eday_votes
+
+print("-------------------")
+print("Dem Election Day Share: ", round(dem_eday_share * 100, 2))
+print("GOP Election Day Share: ", round(gop_eday_share * 100, 2))
+print("Dem Election Day Votes: ", round(results_df["dem_eday_votes"].sum(), 0))
+print("GOP Election Day Votes: ", round(results_df["gop_eday_votes"].sum(), 0))
+print("Total Election Day Votes: ", round(results_df["dem_eday_votes"].sum() + results_df["gop_eday_votes"].sum(), 0))
+print("-------------------\n")
+
+dem_total_share = dem_votes/(dem_votes + gop_votes)
+gop_total_share = gop_votes/(dem_votes + gop_votes)
+
+print("FORECAST")
+print("-------------------")
+print("DEM Share", round(dem_total_share * 100, 2))
+print("GOP Share", round(gop_total_share * 100, 2))
+print("DEM VOTES", round(dem_votes, 0))
+print("GOP VOTES", round(gop_votes, 0))
+print("TOTAL VOTES: ", round(dem_votes + gop_votes, 0))
+margin = round((dem_total_share - gop_total_share) * 100, 2)
+winner = "DEM" if margin > 0 else "GOP"
+print("MARGIN: " + winner + " +" + str(abs(margin)))
+print("-------------------\n")
